f47832604706650a58ee9bca7d9dd4ba1c0b6a4e
Zheyi Zheng <zhengzheyi11@gmail.com>
2024-11-02 19:34:04 -0300

    Minor update on home controller
    Minor update on home controller

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -9,10 +9,15 @@ import Model.SearchForm;
 import play.data.Form;
 import play.mvc.*;
 
+import services.ReadabilityService;
+import services.YTResponse;
+import services.YTRestDir;
 import views.html.Home.display;
 import play.data.FormFactory;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 
 public class HomeController extends Controller {
@@ -42,7 +47,30 @@ public class HomeController extends Controller {
         return ok(display.render(searchForm));
     }
 
+    /**
+     * Basic search page of this application. Use it to display search result. Still need to display search bar
+     * Author: Zheyi Zheng - 40266266
+     * Created: 2024/10/24
+     */
     public Result search(){
+        // Receive submitted keyword
+        // TODO
+        // Create YTRestDir
+        YTRestDir ytRestDir = new YTRestDir();
+        // Get search result. (Replace the following keyword to actual keyword String)
+        CompletableFuture<List<YTResponse>> result =  ytRestDir.searchVideosAsynch(keyword, null, 10);
+        // Create ReadabilityService and get calculation result.
+        ReadabilityService rs = new ReadabilityService(result);
+        List<Double> listOfFRE = rs.getFre();
+        List<Double> listOfFKGL = rs.getFkgl();
+        Double avgFRE = rs.getAvgFRE();
+        Double avgFKGL = rs.getAvgFKGL();
+        // Form search result and information from personal part into one. Store it somewhere(Keep at most 10 search results).
+        // TODO
+        // Create form just like display().
+        // TODO
+        // Pass all information to the view (Including previous search history).
+        // TODO
         return ok();
     }
 }
