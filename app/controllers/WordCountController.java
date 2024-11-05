65eac5794cdd9ac3f6ddb8717b50bb0d6fe5de46
praneetavhad <47533640+praneetavhad@users.noreply.github.com>
2024-11-06 00:39:57 +0530

    Update WordCountController.java

--- app/controllers/WordCountController.java
+++ app/controllers/WordCountController.java
@@ -23,12 +23,22 @@ import java.util.stream.Stream;
      */
 
 public class WordCountController extends Controller {
+    private static final List<String> SKIPWORDS = Arrays.asList(
+            "a", "an", "and", "are", "as", "at", "be", "by", "for", "from", "has", "he",
+            "in", "is", "it", "its", "of", "on", "that", "the", "to", "was", "were", "will",
+            "with", "i", "you", "she", "we", "they", "my", "your", "this", "these", "those",
+            "or", "but", "if", "because", "just","s","t"
+            );
+    
     public Map<String, Long> getWordFrequency(List<YTResponse> videos) {
         return videos.stream()
                 // Extract words from each video's title or description
                 .flatMap(video -> Stream.of((video.getTitle() + " " + video.getDescription()).split("\\W+"))) // Split by non-word characters
                 .map(String::toLowerCase) // Convert to lowercase for case-insensitive counting
                 .filter(word -> !word.isEmpty()) // Remove any empty words
+                .filter(word -> word.length() > 1)           // Remove single-character words
+                .filter(word -> !SKIPWORDS.contains(word))   // Remove unneccasary words
+                .filter(word -> !word.matches("\\d+"))       // Remove numbers
                 .collect(Collectors.groupingBy(word -> word, Collectors.counting())); // Count each word
     }
 
