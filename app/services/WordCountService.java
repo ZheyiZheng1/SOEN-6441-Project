aaacc292c02ca7cce2ad4bb0f8a231027dd1e511
praneetavhad <47533640+praneetavhad@users.noreply.github.com>
2024-11-05 09:59:50 +0530

    Update WordCountService.java
    Added minute changes

--- app/services/WordCountService.java
+++ app/services/WordCountService.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+/**
+     * Added the seperate CompletetableFuture for MoreStat
+     * Author: Praneet Avhad - 40279347
+     * Created: 2024/11/05
+     */
+
 public class WordCountService {
 
     public CompletableFuture<List<YTResponse>> searchVideosAsync(String keyword, String url, String maxResult) throws IOException, NullPointerException {
