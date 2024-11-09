198265dc4be3cc6c2d4b70c96b90b61a14ea4095
Zheyi Zheng <zhengzheyi11@gmail.com>
2024-11-09 15:39:29 -0400

    Update on ReadabilityServiceTest
    Update on ReadabilityServiceTest

--- test/services/ReadabilityServiceTest.java
+++ test/services/ReadabilityServiceTest.java
@@ -20,13 +20,11 @@ public class ReadabilityServiceTest {
 
     /**
      * @author: Zheyi Zheng - 40266266
-     * Created: 2024/10/30
-     * Before each test, create three mock responses, create CompletableFuture for those response then create the readabilityService for calculation.
-     *
+     * Created: 2024/10/29
+     * Test getFreAndValue method. Make sure all calculated value matches what I got from the website.
      */
-    @BeforeEach
-    public void setUp() {
-
+    @Test
+    public void testGetFreAndValue(){
         // Create mock data
         YTResponse first = new YTResponse();
         YTResponse second = new YTResponse();
@@ -45,15 +43,6 @@ public class ReadabilityServiceTest {
 
         // Initialize ReadabilityService with the mock data
         readabilityService = new ReadabilityService(future);
-    }
-
-    /**
-     * @author: Zheyi Zheng - 40266266
-     * Created: 2024/10/29
-     * Test getFreAndValue method. Make sure all calculated value matches what I got from the website.
-     */
-    @Test
-    public void testGetFreAndValue(){
         List<Double> response = readabilityService.getFre();
         List<Double> list = Arrays.asList(83.3, 66.4, 83.3);
         assertEquals(list, response);
@@ -66,6 +55,24 @@ public class ReadabilityServiceTest {
      */
     @Test
     public void testGetFkglAndValue(){
+        // Create mock data
+        YTResponse first = new YTResponse();
+        YTResponse second = new YTResponse();
+        YTResponse third = new YTResponse();
+        // Based on the provided calculator link, fkgl=2.9, fre=83.3
+        first.setDescription("This is the first description.");
+        // Based on the provided calculator link, fkgl=5.2, fre=66.4
+        second.setDescription("This is the second description! This is the second description!");
+        // Based on the provided calculator link, fkgl=2.9, fre=83.3
+        third.setDescription("This is the third description? This is the third description? This is the third description?");
+        // Therefore, the average should be (83.3+66.4+83.3)/3 = 77.6666666667
+        List<YTResponse> mockData = Arrays.asList(first, second, third);
+
+        // Wrap CompletableFuture to the list of YTResponse
+        CompletableFuture<List<YTResponse>> future = CompletableFuture.completedFuture(mockData);
+
+        // Initialize ReadabilityService with the mock data
+        readabilityService = new ReadabilityService(future);
         List<Double> response = readabilityService.getFkgl();
         List<Double> list = Arrays.asList(2.9, 5.2, 2.9);
         assertEquals(list, response);
@@ -78,6 +85,24 @@ public class ReadabilityServiceTest {
      */
     @Test
     public void testGetAvgFRE(){
+        // Create mock data
+        YTResponse first = new YTResponse();
+        YTResponse second = new YTResponse();
+        YTResponse third = new YTResponse();
+        // Based on the provided calculator link, fkgl=2.9, fre=83.3
+        first.setDescription("This is the first description.");
+        // Based on the provided calculator link, fkgl=5.2, fre=66.4
+        second.setDescription("This is the second description! This is the second description!");
+        // Based on the provided calculator link, fkgl=2.9, fre=83.3
+        third.setDescription("This is the third description? This is the third description? This is the third description?");
+        // Therefore, the average should be (83.3+66.4+83.3)/3 = 77.6666666667
+        List<YTResponse> mockData = Arrays.asList(first, second, third);
+
+        // Wrap CompletableFuture to the list of YTResponse
+        CompletableFuture<List<YTResponse>> future = CompletableFuture.completedFuture(mockData);
+
+        // Initialize ReadabilityService with the mock data
+        readabilityService = new ReadabilityService(future);
         double response = readabilityService.getAvgFRE();
         assertEquals((83.3+66.4+83.3)/3, response);
     }
@@ -89,6 +114,24 @@ public class ReadabilityServiceTest {
      */
     @Test
     public void testGetAvgFKGL(){
+        // Create mock data
+        YTResponse first = new YTResponse();
+        YTResponse second = new YTResponse();
+        YTResponse third = new YTResponse();
+        // Based on the provided calculator link, fkgl=2.9, fre=83.3
+        first.setDescription("This is the first description.");
+        // Based on the provided calculator link, fkgl=5.2, fre=66.4
+        second.setDescription("This is the second description! This is the second description!");
+        // Based on the provided calculator link, fkgl=2.9, fre=83.3
+        third.setDescription("This is the third description? This is the third description? This is the third description?");
+        // Therefore, the average should be (83.3+66.4+83.3)/3 = 77.6666666667
+        List<YTResponse> mockData = Arrays.asList(first, second, third);
+
+        // Wrap CompletableFuture to the list of YTResponse
+        CompletableFuture<List<YTResponse>> future = CompletableFuture.completedFuture(mockData);
+
+        // Initialize ReadabilityService with the mock data
+        readabilityService = new ReadabilityService(future);
         double response = readabilityService.getAvgFKGL();
         assertEquals((2.9+5.2+2.9)/3, response);
     }
