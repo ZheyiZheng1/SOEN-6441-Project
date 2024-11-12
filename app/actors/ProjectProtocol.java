e5f80a1e087c15a233d5057e0292a7f603b8e389
Zheyi Zheng <zhengzheyi11@gmail.com>
2024-11-11 21:56:07 -0400

    Dependency of Akka is added.
    Dependency of Akka is added. Also created ProjectProtocol class and APIActor class.

---
+++ app/actors/ProjectProtocol.java
@@ -0,0 +1,26 @@
+package actors;
+/**
+ * @author: Zheyi Zheng - 40266266
+ * Created: 2024/11/11
+ * This is the ProjectProtocol class. This class contains all the messages classes.
+ */
+public class ProjectProtocol {
+    /**
+     * @author: Zheyi Zheng - 40266266
+     * Created: 2024/11/11
+     * This is the KeyWordSearch class. This is a message class that only pass keyword.
+     * This class only gets used when controller want to pass keyword to actor to perform search action.
+     */
+    public static class KeyWordSearch {
+        public final String keyword;
+        /**
+         * @author: Zheyi Zheng - 40266266
+         * Created: 2024/11/11
+         * Constructor method.
+         * @param keyword string of keyword that user want to search.
+         */
+        public KeyWordSearch(String keyword) {
+            this.keyword = keyword;
+        }
+    }
+}
